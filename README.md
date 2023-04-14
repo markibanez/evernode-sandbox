2e3be468217ce28733e324c1660ce7d23b33d6ed
Mark Ibanez <mark.ibanez@gmail.com>
2023-04-14 09:22:30 +0800

    Starter kit

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # evernode-sandbox
 Sandbox to play with Evernode
+
+# Getting Started
+1. Install Docker https://docs.docker.com/engine/install/
+2. Install NodeJS https://nodejs.org/en/
+3. Make sure Docker daemon is running
+4. Install HotPocket developer kit globally with `npm i hpdevkit -g`
+5. Create a HotPocket smart contract with `hpdevkit gen nodejs starter-contract hello`. In this case, we're creating a smart contract called `hello`.
+6. Cd into the contract directory with `cd hello` and run `npm install`, then `npm start`
+7. If everything went well, you should see logs in the terminal like
+```
+20230414 01:13:26.094 [inf][hpc] Not enough peers proposing to perform consensus. votes:2 needed:3
+20230414 01:13:27.852 [inf][hpc] ****Ledger created**** (lcl:315-68f3a2f8 state:cd37be02 patch:61261ed3)
+20230414 01:13:28.980 [inf][hpc] ****Ledger created**** (lcl:316-86159375 state:cd37be02 patch:61261ed3)
+20230414 01:13:30.853 [inf][hpc] ****Ledger created**** (lcl:317-10ca9a51 state:cd37be02 patch:61261ed3)
+```
+8. Create a HotPocket client app with `hpdevkit gen nodejs blank-client hello-client`. In this case, we're creating a client app called `hello-client`.
+9. Cd into the client app directory with `cd hello-client` and run `npm install`, then `node hello-client.js`
+10. If everything went well, you should see logs in the terminal like
+```
+Connecting to wss://localhost:8081
+Connected to wss://localhost:8081
+HotPocket Connected.
+```
---
+++ hello/src/hello.js
@@ -0,0 +1,60 @@
+const fs = require('fs').promises;
+
+// This sample application writes and reads from a simple text file to serve user requests.
+// Real-world applications may use a proper local database like sqlite.
+const dataFile = 'datafile.txt'
+
+export class hello {
+    sendOutput; // This function must be wired up by the caller.
+
+    async handleRequest(user, message, isReadOnly) {
+
+        // This sample application defines two simple messages. 'get' and 'set'.
+        // It's up to the application to decide the structure and contents of messages.
+
+        if (message.type == 'get') {
+
+            // Retrieved previously saved data and return to the user.
+            const data = await this.getData();
+            await this.sendOutput(user, {
+                type: 'data_result',
+                data: data
+            })
+        }
+        else if (message.type == 'set') {
+
+            if (!isReadOnly) {
+                // Save the provided data into storage.
+                await this.setData(message.data);
+            }
+            else {
+                await this.sendOutput(user, {
+                    type: 'error',
+                    error: 'Set data not supported in readonly mode'
+                })
+            }
+
+        }
+        else {
+            await this.sendOutput(user, {
+                type: 'error',
+                error: 'Unknown message type'
+            })
+        }
+    }
+
+    async setData(data) {
+        // HotPocket subjects data-on-disk to consensus.
+        await fs.writeFile(dataFile, data);
+    }
+
+    async getData() {
+        try {
+            return (await fs.readFile(dataFile)).toString();
+        }
+        catch {
+            console.log('Data file not created yet. Returning empty data.');
+            return '';
+        }
+    }
+}
